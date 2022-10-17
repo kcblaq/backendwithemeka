eda5ee121475b468e8c41b388eadd33f78d05c02
kcblack22@gmail.com <kcblack22@gmail.com>
2022-10-17 22:04:40 +0100

    Javascript assignment

--- add.js
+++ add.js
@@ -1,7 +1,7 @@
-let x = {}
-console.log(typeof x)
+// let x = {}
+// console.log(typeof x)
 
-let description = " is learning coding"
+// let description = " is learning coding"
 //I am trying to add three numbers here
 
 /* here is my comment 
@@ -9,11 +9,44 @@ and yet another comment
 and yet 
 another */
 
-function SayNameandDescription(a){
-    return a +  description
-}
-
-console.log(SayNameandDescription("Chuka"))
+// function SayNameandDescription(a){
+//     return a +  description
+// }
 
 
+
+
+
+// let x = 3 //Number
+// let y = "3" //string
+
+//when we have two equal (==) they are the same here ...
+// When we have 3 equal(===) they are not the same here...
+// if(x === y) {
+//     console.log("They are the same")
+// } else {
+//     console.log("They are not the same")
+// }
+
+
+// ASSIGNMENT OPERATORS
+// let x =3; y = 5; z = 30; j = "I have my farm"
+
+// let jay = x -= x;
+
+// let name = " here is my name"
+// let question = " What is your name"
+// const detail = question + name
+
+
+// let jay = false
+
+// if (!jay){
+//     console.log("I am first")
+// } else {
+//     console.log("Second to run if false")
+// }
+
+let x = 7
+console.log(--x)
 
