634c8f5e78d5e8fb478fe90fec0918be499d597d
kcblack22@gmail.com <kcblack22@gmail.com>
2022-10-17 20:57:43 +0100

    first commit

---
+++ add.js
@@ -0,0 +1,19 @@
+let x = {}
+console.log(typeof x)
+
+let description = " is learning coding"
+//I am trying to add three numbers here
+
+/* here is my comment 
+and yet another comment 
+and yet 
+another */
+
+function SayNameandDescription(a){
+    return a +  description
+}
+
+console.log(SayNameandDescription("Chuka"))
+
+
+
