e98f3b84606f2564d70a8a6974df04d78f6cbb6f
kcblack22@gmail.com <kcblack22@gmail.com>
2022-10-21 22:19:00 +0100

    More

--- add.js
+++ add.js
@@ -1,6 +1,8 @@
 // let x = {}
 // console.log(typeof x)
 
+const { log } = require("console");
+
 // let description = " is learning coding"
 //I am trying to add three numbers here
 
@@ -47,6 +49,62 @@ another */
 //     console.log("Second to run if false")
 // }
 
-let x = 7
-console.log(--x)
+// let x = 7
+// console.log(--x)
 
+// const demo = [
+//     {name: "Jay", address:'Lagos'},
+//     {name: "Jay", address:'Lagos'},
+//     {name: "Jay", address:'Lagos'},
+//     {name: "Jay", address:'Lagos'},
+//     {name: "Jay", address:'Lagos'},
+//     {name: "Jay", address:'Lagos'},
+//     {name: "Jay", address:'Lagos'}
+// ]
+
+
+
+// function SayMyName(name){
+//     name = name.toLowerCase()
+//     name = "My Dear" + " " +name
+//     return name
+// }
+
+// function GreetMe (){
+//     return " Hello"  + " " + SayMyName("Amaka")
+// }
+
+//Assignment 1
+// const NigeriaTimeToAmerica = (time ) =>{
+// return time - 7
+// }
+
+// console.log(NigeriaTimeToAmerica(12))
+
+// let students = ["Amaka", "Ndu", "Jake"]
+
+// const person = {
+//     firstName: "Lambart",
+//     lastName: "Lugard",
+//     age: 50,
+//     eyeColor: "red",
+//     getfullname: function getFullName(){
+//         return this.firstName + " " + this.lastName
+//     }
+//   };
+
+//   console.log(person.getfullname())
+
+
+// let str = "4562445tte56uyt5ety644645"
+// console.log(str.length)
+
+const letter = "i have a new pair of shoe that is double my size"
+let leta = "           jerry is back    "
+console.log(leta.trim())
+
+// console.log(letter)
+
+
+
+  
